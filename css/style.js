c778fdddeb0747f91256c52cb30ba84cd2cf6b63
Airdropcel <muhamadrivaioke@gmail.com>
2026-07-31 22:47:57 +0700

    Create css/style.js

---
+++ css/style.js
@@ -0,0 +1,68 @@
+/* ==========================================
+   TAHU GEJROT PAKDE BURUNG
+   STYLE.CSS
+========================================== */
+
+/* Google Font */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap');
+
+/* RESET */
+
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+
+html{
+    scroll-behavior:smooth;
+}
+
+body{
+    font-family:'Poppins',sans-serif;
+    background:#fafafa;
+    color:#222;
+    overflow-x:hidden;
+    line-height:1.6;
+}
+
+img{
+    max-width:100%;
+    display:block;
+}
+
+a{
+    text-decoration:none;
+}
+
+ul{
+    list-style:none;
+}
+
+/* WARNA */
+
+:root{
+
+--primary:#c62828;
+--secondary:#ff9800;
+--dark:#111;
+--light:#fff;
+--gray:#777;
+--bg:#fafafa;
+
+--shadow:
+0 10px 30px rgba(0,0,0,.12);
+
+--radius:14px;
+
+}
+
+/* CONTAINER */
+
+.container{
+
+width:90%;
+max-width:1200px;
+margin:auto;
+
+}
