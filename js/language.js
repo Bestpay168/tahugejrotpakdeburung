364e233b2ce40b2c369c96463b753eb9984400be
Airdropcel <muhamadrivaioke@gmail.com>
2026-08-12 06:34:48 +0700

    Create language.js

---
+++ js/language.js
@@ -0,0 +1,9 @@
+const langBtn=document.getElementById("langBtn");
+
+const langMenu=document.querySelector(".lang-menu");
+
+langBtn.onclick=function(){
+
+    langMenu.classList.toggle("show");
+
+}
